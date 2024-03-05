ec6eb1c014d259b4dc0137b8f5aa68616272afeb
Sampul-CodeMine <studysampul@gmail.com>
2024-03-05 19:25:01 +0100

    Complete Code for all problems

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (err) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
+}
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(`${err.name}: ${err.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
