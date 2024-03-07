f11309c60b250005946fd695a8196645374c7bfe
Sampul-CodeMine <studysampul@gmail.com>
2024-03-07 18:52:15 +0100

    Updated Solution to Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,21 +2,19 @@
 
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    if (typeof sqft !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+
     if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
+      if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
       }
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
   }
 }
