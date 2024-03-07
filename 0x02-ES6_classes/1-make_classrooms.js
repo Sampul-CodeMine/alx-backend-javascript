d19bba8a190d92e401589c921c9075836eeef6e2
Sampul-CodeMine <studysampul@gmail.com>
2024-03-07 17:38:15 +0100

    Restructured some codes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,8 @@
 
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
+function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
+
+export default initializeRooms;
