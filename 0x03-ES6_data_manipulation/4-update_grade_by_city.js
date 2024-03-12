79d3f8e6bdf6a136c5d4ad9974a2f678b12088db
Sampul-CodeMine <studysampul@gmail.com>
2024-03-12 12:07:17 +0100

    Solution to Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,7 +4,7 @@
  * This is a function that returns the sum of the ids of all the students
  * @param {Array} studarr an array of objects of students
  * @param {String} city the city of the student
- * @param {Array} newGrade an array of objects of new grades  
+ * @param {Array} newGrade an array of objects of new grades
  * @returns An array of students for a specific city with their new grades.
  */
 
@@ -16,7 +16,6 @@ export default function updateStudentGradeByCity(studarr, city, newGrade) {
     id: student.id,
     firstName: student.firstName,
     location: student.location,
-    grade: (newGrade.filter((grade) => grade.studentId === student.id)
-    .pop() || { grade: 'N/A' }).grade, 
+    grade: (newGrade.filter((grade) => grade.studentId === student.id).pop() || { grade: 'N/A' }).grade,
   }));
 }
