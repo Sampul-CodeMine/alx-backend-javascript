49c317cc568f5adb915da66611211cf42b17b807
Sampul-CodeMine <studysampul@gmail.com>
2024-03-12 16:09:58 +0100

    Solution to Task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+/**
+ * This is a function to changes the quantity of a specified item to 100
+ * @param {Map} map - map of a grocert name and quantity
+ * @returns Nothing
+ */
+
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    map.forEach((valu, key) => {
+      if (valu === 1) {
+        map.set(key, 100);
+      }
+    });
+  }
+  throw new Error('Cannot process');
+}
