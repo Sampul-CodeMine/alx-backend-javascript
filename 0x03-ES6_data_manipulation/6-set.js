1e35f91b8f5cfe42df998a762e65d36c2eab8f8a
Sampul-CodeMine <studysampul@gmail.com>
2024-03-12 15:29:47 +0100

    Solution to Task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * This is a function to create a set from an array
+ * @param {Array} arr - Source array
+ * @returns a set
+ */
+
+export default function serFromArray(arr) {
+  return new Set(arr);
+}
